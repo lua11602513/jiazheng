865d2bb5905c2654d7552b285c92178a9d2bde1e
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-09-14 11:15:30 +0800

    Add files via upload

---
+++ ch801.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package work1111;
+
+class Caaa{
+	int a, b, c;
+}
+public class ch801 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Caaa obj;
+		obj = new Caaa();
+		obj.a = 5;
+		obj.b = 3;
+		obj.c = obj.a * obj.b;
+		System.out.print("a = " +obj.a+ "" + "  b = "+obj.b+ "" + " c= " +obj.c);
+	}
+
+}
