40048c3e2e27c6d3a4859197cdd4582411cbc959
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-03 00:43:09 +0800

    Add files via upload

---
+++ java06.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package java08;
+class CCircle {
+	double pi = 3.14;
+	double radius;
+
+	void show_periphery() {
+		System.out.println("periphety=" + 2 * pi * radius);
+	}
+}
+	public class java06 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			CCircle cir1 = new CCircle();
+			cir1.radius = 3.0;
+			cir1.show_periphery();
+
+		}
+
+	}
