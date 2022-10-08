9328fff680fd5c5a8c25ddf14536f52410dc3693
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-08 22:12:18 +0800

    Add files via upload

---
+++ class09.java
@@ -0,0 +1,33 @@
+package java08;
+
+class CCalculator {
+	int a; int b; int c;
+	void set_value(int x, int y, int z) {
+		x = 25; y = 3; z = 7;
+	}
+	void show() {
+		System.out.println(a+" "+b+" "+c);
+	}
+	int add() {
+		return a+b+c;
+	}
+	int sub() {
+		return a-b-c;
+	}
+	int mul() {
+		return a*b*c;
+	}
+	int avg() {
+		return(a+b+c)/3;
+	}
+}
+
+	public class class09{
+
+		public static void main(String[] args) {
+			CCalculator obj = new CCalculator();
+			obj.a = 25; obj.b = 3; obj.c = 7;
+			obj.show();
+			
+}
+	}
